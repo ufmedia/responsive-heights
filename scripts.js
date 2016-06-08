446a846e228036c405e42c334f183388e5e08f4b
ufmedia <john@ufmedia.co.uk>
2016-06-08 11:45:39 +0100

    Responsive Heights

---
+++ scripts.js
@@ -0,0 +1,39 @@
+//On document load
+jQuery(window).on("load", function($) {
+	responsiveHeights();
+	
+});
+
+//Resize Events
+jQuery(window).resize(function() {
+	responsiveHeights();
+});
+
+//Kick ass responsive height function
+function responsiveHeights() {
+    jQuery('.res-height-container').each(function() {
+        jQuery(this).find('.res-height').height('auto');
+        var startPoint = jQuery(this).attr('data-start-point');
+        var endPoint = jQuery(this).attr('data-end-point');
+        if (typeof startPoint == 'undefined') {
+            startPoint = 768;
+        }
+        if (typeof endPoint == 'undefined') {
+            endPoint = 5001;
+        }
+        if (jQuery(window).width() > startPoint && jQuery(window).width() < endPoint) {
+            var highestBox = 0;
+            jQuery(this).find('.res-height').each(function() {
+                if (jQuery(this).height() > highestBox) {
+                    highestBox = jQuery(this).height();
+                }
+            })
+            jQuery(this).find('.res-height').height(highestBox);
+        }
+    });
+}
+
+
+
+
+
