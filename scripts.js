536374c04591e890e295f32939c32a8cc57e69f2
ufmedia <john@ufmedia.co.uk>
2016-07-27 13:23:55 +0100

    Comments added

--- scripts.js
+++ scripts.js
@@ -1,7 +1,6 @@
 //On document load
 jQuery(window).on("load", function($) {
 	responsiveHeights();
-	
 });
 
 //Resize Events
@@ -11,29 +10,48 @@ jQuery(window).resize(function() {
 
 //Kick ass responsive height function
 function responsiveHeights() {
+	
+	//Find all res-height-containers
     jQuery('.res-height-container').each(function() {
+		
+		//Reset all res-height objects
         jQuery(this).find('.res-height').height('auto');
+		
+		//Set break points
         var startPoint = jQuery(this).attr('data-start-point');
         var endPoint = jQuery(this).attr('data-end-point');
+		
+		//Use default breakpoints if none set
         if (typeof startPoint == 'undefined') {
             startPoint = 768;
         }
         if (typeof endPoint == 'undefined') {
             endPoint = 5001;
         }
+		
+		//If the window width is within the break points, loop through res-height objects
         if (jQuery(window).width() > startPoint && jQuery(window).width() < endPoint) {
             var highestBox = 0;
             jQuery(this).find('.res-height').each(function() {
-                if (jQuery(this).height() > highestBox) {
-                    highestBox = jQuery(this).height();
+                if (jQuery(this).outerHeight(true) > highestBox) {
+					
+					//We keep adjusting this var until we have the heighest res-height object
+                    highestBox = jQuery(this).outerHeight(true);
                 }
             })
-            jQuery(this).find('.res-height').height(highestBox);
+
+			//Set the res-height object to the highestBox height
+			//If a res-height object has the res-height-master class we leave it alone.
+			jQuery(this).find('.res-height:not(.res-height-master)').outerHeight(highestBox);
+			
+			//res-height-child objects don't have their height taken into account above but will be set to the heighestBox height
+			//usefully when you need to shrink and object (specifically been used with iframes).
+			jQuery(this).find('.res-height-child').height(highestBox);
         }
     });
 }
 
-//Test
+
 
 
 
